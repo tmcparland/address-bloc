6f7c8b139847fe9f7af867537c9c80c41515b87a
Tara McParland <tara.mcparland@gmail.com>
2016-07-12 04:10:29 +0000

    assignment-22-searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -50,6 +50,18 @@
        end
    
    return nil
+   
+   end
+   
+      def iterative_search(name)
+         @entries.each do |entry|
+             if entry.name == name
+                 return entry
+             end
+         end
+         
+         return nil
+   
    end
    
  end
